ccacebc26dc1145a5b302dea28943e275e78d3df
elena20ruiz <elena20ruiz@github.com>
2018-07-24 22:24:40 +0200

    Change to JSON format and start to implement sponsors responses

--- hackupc/bienebot/responses/sponsors/sponsors.py
+++ hackupc/bienebot/responses/sponsors/sponsors.py
@@ -1,4 +1,4 @@
-import random
+import random, json
 from hackupc.bienebot.responses.sponsors import sponsors_data
 
 
@@ -6,5 +6,25 @@ def get_message(response_type):
     """
     Return a message from a sponsor intent
     """
+    with open('sponsors_data.json') as f:
+        data = json.load(f)
 
-    return "You asked me about sponsors"
+    intent = response_data['topScoringIntent']['intent']
+    entities = response_data['entities']
+    list_intent = intent.split('.')
+
+    switcher = {
+        'which': which,
+        'where': where
+    }
+    # Get the function from switcher dictionary
+    func = switcher.get(list_intent[1], lambda: "No understand" )
+    # Execute the function
+    return func(data,intent,entities)
+
+def which(data,intent,entities):
+    return data['which']
+
+def where(data,intent,entities):
+    sponsor = entities['0']['entity']
+    return data['where'][sponsor]
