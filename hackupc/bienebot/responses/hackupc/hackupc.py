747ad1e515aeda5d3abf78bf3b325a02c1c4ee67
Albert Suarez <alsumo95@gmail.com>
2018-10-17 23:27:58 +0200

    add schedule and what's next response

--- hackupc/bienebot/responses/hackupc/hackupc.py
+++ hackupc/bienebot/responses/hackupc/hackupc.py
@@ -1,5 +1,8 @@
 import json
 import random
+import requests
+
+from datetime import datetime, timedelta
 
 from hackupc.bienebot.util import log
 
@@ -18,5 +21,43 @@ def get_message(response_type):
         # Log stuff
         log.info('|RESPONSE| Looking for [{}] from JSON element'.format(list_intent[1]))
 
-        array = [random.choice(data[list_intent[1]])]
+        if list_intent[1] == 'Next':
+            array = next_hackupc()
+        elif list_intent[1] == 'Schedule':
+            array = ['\n'.join(data['Schedule'])]
+        else:
+            array = [random.choice(data[list_intent[1]])]
+
         return array
+
+
+def next_hackupc():
+    """
+    Get next event calling Live json
+    :return: response
+    """
+    now = datetime.utcnow() + timedelta(hours=2)
+    params = {
+        'date': now.time()
+    }
+    response = requests.get(url='https://hackupc.com/assets/data/schedule.json', params=params, timeout=5)
+    response_dict = response.json()
+
+    # Per each day
+    for day in response_dict['days']:
+        day_date = datetime.strptime(day['date'], '%d/%m/%Y')
+        if now.date() <= day_date.date():
+            for event in day['events']:
+                event_date = datetime.strptime('{} {}'.format(day['date'], event['startHour']), '%d/%m/%Y %H:%M')
+                if now <= event_date:
+                    return ['The following event is called `{}` located at `{}`. {}. It starts at `{}` and ends at `{}`.'
+                            .format(
+                                    event['title'],
+                                    event['locationName'],
+                                    event['description'],
+                                    event['startHour'],
+                                    event['endHour']
+                            )]
+
+    # No more activities response
+    return ['There are not more activities in this edition of HackUPC :hackupc: So excited to see you again next year!']
