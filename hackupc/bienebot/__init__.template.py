af3f97ffd5b095d0b8f58ae02fb1828b43a70274
Arnau Casas <33383702+Casassarnau@users.noreply.github.com>
2022-04-23 16:29:40 +0200

    Added function to deactivate

--- hackupc/bienebot/__init__.template.py
+++ hackupc/bienebot/__init__.template.py
@@ -1,9 +1,9 @@
 # Slack API
 SLACK_API_TOKEN = ''  # Token for Biene bot
 SLACK_API_METHOD = 'chat.postMessage'
-SLACK_API_CHANNEL = '' # Get the ID of the channel that the bot wil send help (answering quesitions)
+SLACK_API_ACTIVE = True
 SLACK_API_RANDOM = '' # The ID of the channel with topic random (only BIENE responses)
-SLACK_API_ORGANIZERS = ''
+SLACK_API_ORGANIZERS = '' # Get the ID of the channel that the bot wil manage organizer commands
 
 # Luis stuff
 LUIS_SUBSCRIPTION_KEY = '' # Key 1 without dashes
